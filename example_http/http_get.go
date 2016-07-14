d20cc8fdda1bff9ee80f1c791fd3d2b52c122012
yang.yue <yang.yue@baifendian.com>
2016-07-14 19:54:19 +0800

    A simple HTTP GET func

---
+++ example_http/http_get.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+import (
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+	content := HttpGet("http://www.google.com")
+	fmt.Println(content)
+}
+
+func HttpGet(urls string) (x string) {
+	resp, err := http.Get(urls)
+	if err != nil {
+		fmt.Println("Http Get has some error!")
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read io error!")
+		return ""
+	}
+
+	return string(body)
+}
+
